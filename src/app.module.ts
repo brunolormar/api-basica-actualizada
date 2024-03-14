eea9bbc0bd12b075c423ff0f2d89415f4df3e2f1
brunolormar <brunolorfpgs@gmail.com>
2024-03-14 01:15:40 +0100

    Actualizacion antes del examen del 2 trimestre

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { ConfigModule } from '@nestjs/config';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: +process.env.DBPORT,
+      port: +process.env.DB_PORT,
       database: process.env.DB_NAME,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
